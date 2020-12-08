9804590943190d38c251261c8bd79af4c52523c0
rkmcodes <rkmcodes@gmail.com>
2020-12-07 20:45:01 -0600

    Initial commit - started designing classes Menu & MenuItem

---
+++ src/com/company/Main.java
@@ -0,0 +1,84 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
+
+/*
+
+The menu consists of several menu items
+Each menu item has a price, description, and category (appetizer, main course, or dessert)
+It should be possible to display whether a menu item is new or not
+The app should know when the menu was last updated, so visitors can see that the restaurant is constantly changing and adding exciting new items
+
+* Class Menu *
+
+ArrayList
+
+Fields:
+
+not sure...
+
+Constructors:
+
+diff btw meth & constructors
+constructors: used for instantiating (making a new thing) & is named the same as the class
+when to make a custom constructor?: when you know all the fields, depends on the data you have/need when creating the class
+
+
+Methods:
+    isNew(String menuItem){
+    determines whether menu item is new or not
+    }
+
+    isUpdated(not sure what param){
+    shows whether menu has been updated?
+    }
+
+* Class MenuItem *
+
+Fields:
+    - price
+        type: Float (F because it needs to be obj type?)
+        access: private (but will it change?)
+    - description
+        type: String
+        access: private (but will it change?)
+    - appetizer category?
+        type: boo
+        access: private
+    - main course category?
+        type: boo
+        access: private
+    - dessert category?
+        type: boo
+        access: private
+
+make category into array?
+category as field with category names inside
+
+Contructors:
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ */
